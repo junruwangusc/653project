6193d8b00b003a339273f8139e3932b1126350bf
junruwangusc <60276749+junruwangusc@users.noreply.github.com>
2020-11-12 20:36:54 -0800

    添加生成result vector地址功能
    添加生成result vector地址功能
    下一步根据目前代码，实现Cache功能，最后产生正确的memory trace

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -21,7 +21,7 @@ def memory_start_address():
         result_start_addr.append(int(i*blocksize+result_offset))
     return matrix_start_addr, vector_start_addr, result_start_addr
 
-def output_processed_file(file_name, block_index, r_c_addr):
+def output_processed_file(file_name, block_index, r_c_addr, first_row_in_block):
     file_name_without_extension=file_name.split(".")[0]
     output_dir = "../proceeded_data_sets/"+file_name_without_extension
     if not os.path.exists(output_dir):
@@ -30,14 +30,18 @@ def output_processed_file(file_name, block_index, r_c_addr):
     out_file = open(output_file_name, mode='w')
     for data in r_c_addr:
         print(data[0], data[1], data[2], file=out_file)
+    output_file_name = output_dir+"/"+file_name_without_extension+"_firstrowinblock"+".txt"
+    out_file = open(output_file_name, mode='w')
+    for data in first_row_in_block:
+        print(data, file=out_file)
     return 0
 
 def output_generated_vector(file_name, block_index, r_v_addr):
-    file_name_without_extension=file_name.split(".")[0]+"_vector"
+    file_name_without_extension=file_name.split(".")[0]
     output_dir = "../proceeded_data_sets/"+file_name_without_extension
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    output_file_name = output_dir+"/"+file_name_without_extension+"_"+str(block_index)+".txt"    
+    output_file_name = output_dir+"/"+file_name_without_extension+"_vector_"+str(block_index)+".txt"    
     out_file = open(output_file_name, mode='w')
     for data in r_v_addr:
         print(data[0], data[1], data[2], file=out_file)
@@ -56,31 +60,41 @@ def readin(file_name):
             edges.append((int(lineData[0]),int(lineData[1])))
     edges = sorted(edges, key=operator.itemgetter(0,1))
     #sort the edges first by the row then the column
+    non_zero_num = len(edges)#edges are also the number of non-zeros
     row_num = edges[len(edges)-1][0] + 1 # 0, 1, .... max row index
-    row_block_size = row_num // channel_num #round down
+    row_block_size = non_zero_num // channel_num #round down, number if elements
+    vec_block_size = row_num // channel_num
+    non_zero_thrshld = []
     row_thrshld = []
     for i in range(channel_num-1):
-        row_thrshld.append((i+1)*row_block_size)
+        non_zero_thrshld.append((i+1)*row_block_size)
+        row_thrshld.append((i+1)*vec_block_size)
         # totally 15 threshold needed
-    print("row_thrshld=",row_thrshld)
+    print("non_zero_thrshld=",non_zero_thrshld)
     matrix_start_addr, vector_start_addr, result_start_addr = memory_start_address()
     #---------------------------------------------------------------------------------
     # matrix partiton 
     current_block = 0
     r_c_addr = [] # make a new list of row column and address
     accum_count = 0 # the order of the edge in the corresponding block
+    accum_non_zero = 0 # the order of the edge among all the non zeros
+    last_row = -1 # last row
+    first_row_in_block = [0]
     for edge in edges: # edge is a tuple
         if(current_block != (channel_num-1)):
-            if(edge[0] >= row_thrshld[current_block]):
-                #print("current_block=",current_block)
-                accum_count = 0
-                output_processed_file(file_name, current_block, r_c_addr)
-                r_c_addr.clear() # clear the block data
-                current_block += 1
+            if(edge[0] != last_row): # the row changes
+                if(accum_non_zero >= non_zero_thrshld[current_block]):
+                    #print("current_block=",current_block)
+                    first_row_in_block.append(edge[0])
+                    accum_count = 0
+                    output_processed_file(file_name, current_block, r_c_addr, first_row_in_block)
+                    r_c_addr.clear() # clear the block data
+                    current_block += 1
+        accum_non_zero += 1
         addr_t = accum_count*16 + matrix_start_addr[current_block] # each matrix data needs 16 bytes memory space
         accum_count += 1
         r_c_addr.append((edge[0], edge[1], addr_t))
-    output_processed_file(file_name, current_block, r_c_addr)
+    output_processed_file(file_name, current_block, r_c_addr, first_row_in_block)
     #---------------------------------------------------------------------------------
     # vector generation
     current_block = 0
--- src/result_address_calc.py
+++ src/result_address_calc.py
@@ -10,23 +10,25 @@ channel_num = 16 #define the channel num according to HBM
 blocksize = 536870912
 vector_offset = blocksize * 30 // 32
 result_offset = blocksize * 31 // 32
+result_start_addr = []
 
-def result_start_addr_calc():
-    result_start_addr = [] # all in decimal
-    for i in range(channel_num):
-        result_start_addr.append(int(i*blocksize+result_offset))
-    return result_start_addr
-   
+def result_addr_generate(result_val, block_num, row_num, fisrt_row_of_the_block):
+    print("block_num=",block_num)
+    index = row_num - fisrt_row_of_the_block
+    block_start_addr = int(block_num*blocksize+result_offset)
+    addr_t = block_start_addr + index*8 # a result vector needs to store the value and row 
+    return row_num, result_val, addr_t   
 
 def readin(file_name):
     matrix_block_data = []
     #vector_total_Data = []
-    vector_row_value = {} # dictionary
-    vector_row_addr = {} # dictionary
+    vector_row_value_dict = {} # dictionary
+    vector_row_addr_dict = {} # dictionary
+    first_row_in_block = []
     file_name_without_extension=file_name.split(".")[0]
     for i in range(0,channel_num):
         matrix_file_path = "../proceeded_data_sets/" + file_name_without_extension
-        vector_file_path = "../proceeded_data_sets/" + file_name_without_extension + "_vector"
+        vector_file_path = "../proceeded_data_sets/" + file_name_without_extension
         matrix_file = open(matrix_file_path+"/"+file_name_without_extension+"_"+str(i)+".txt", mode='r')
         vector_file = open(vector_file_path+"/"+file_name_without_extension+"_vector"+"_"+str(i)+".txt", mode='r')
         temp_matrix_block = []
@@ -42,14 +44,19 @@ def readin(file_name):
             lineData=line.split(' ')    # split by " ", r_c_addr
             nums=[int(num) for num in lineData]
             #vector_total_Data.append(lineData)  # r_v_addr
-            vector_row_value[nums[0]] = nums[1]
-            vector_row_addr[nums[0]] = nums[2]
+            vector_row_value_dict[nums[0]] = nums[1]
+            vector_row_addr_dict[nums[0]] = nums[2]
+    firstrowinblock_file = open("../proceeded_data_sets/"+file_name_without_extension+"/"+file_name_without_extension+"_firstrowinblock.txt", mode='r')
+    lines=firstrowinblock_file.read().splitlines()
+    for line in lines:
+            nums=int(line)    # split by " ", r_c_addr
+            first_row_in_block.append(nums)    
     # read files finished
-    return matrix_block_data, vector_row_value, vector_row_addr
+    return matrix_block_data, vector_row_value_dict, vector_row_addr_dict, first_row_in_block
 
 
-def process_element(matrix_block_data, vector_row_value, vector_row_addr):
-    total_row_num = len(vector_row_value)
+def process_element(matrix_block_data, vector_row_value_dict, vector_row_addr_dict, fisrt_row_of_blocks):
+    total_row_num = len(vector_row_value_dict)
     matrix_ptr = 0 # matrix data index in each block
 
     last_row = [-1 for i in range(channel_num)] # record last row in block 0~15
@@ -57,7 +64,7 @@ def process_element(matrix_block_data, vector_row_value, vector_row_addr):
     result_val = [0 for i in range(total_row_num)] # record result vector
     result_row_finish = [0 for i in range(total_row_num)] # record whether the result of the row is finished
     visited = [0 for i in range(total_row_num)] # avoid repetition subtraction
-    
+    result_out = []
     not_finished = 1
     while not_finished:
         temp_matrix_data = [] # r_c_addr
@@ -83,8 +90,8 @@ def process_element(matrix_block_data, vector_row_value, vector_row_addr):
         for i in range(0,channel_num):
             temp_row = temp_matrix_data[i][0]
             if(temp_row != -1): # if the row exists
-                vec_value_temp = vector_row_value[temp_row] # get the corresponding data
-                vec_addr_temp = vector_row_addr[temp_row] # get the corresponding addr
+                vec_value_temp = vector_row_value_dict[temp_row] # get the corresponding data
+                vec_addr_temp = vector_row_addr_dict[temp_row] # get the corresponding addr
                 vector_value.append(vec_value_temp)
                 if vec_addr_temp not in vector_addr:
                     vector_addr.append(vec_addr_temp)
@@ -92,8 +99,9 @@ def process_element(matrix_block_data, vector_row_value, vector_row_addr):
 
                 if( (current_row[i]!=last_row[i])and(last_row[i]!=-1) ): # if current_row != last_row and last_row exist
                     result_row_finish[last_row[i]] = 1 # since the last row is not the same as the current row
-                    print("result of row ",last_row[i]," is finished,","current_row=",current_row[i],"last_row=",last_row[i])
+                    #print("result of row ",last_row[i]," is finished,","current_row=",current_row[i],"last_row=",last_row[i])
                     print("result_val=",result_val[last_row[i]])
+                    result_out.append(result_addr_generate(result_val[last_row[i]], i, last_row[i], fisrt_row_of_blocks[i]))
             else: # if the row doesn't exist
                 if(result_row_finish[last_row[i]]==0):
                     result_row_finish[last_row[i]] = 1
@@ -103,24 +111,34 @@ def process_element(matrix_block_data, vector_row_value, vector_row_addr):
             if( (current_row[i]==last_row[i]) and last_row[i]!=-1):
                 if(result_row_finish[last_row[i]]==1 and visited[i]==0):
                     visited[i] = 1
-                    print("block ",i," is finished at row",last_row[i])
+                    #print("block ",i," is finished at row",last_row[i])
                     print("result_val=",result_val[last_row[i]])
-        #if matrix_ptr<100:
-        #print("random access of vector address: ", vector_addr)
+                    result_out.append(result_addr_generate(result_val[last_row[i]], i, last_row[i], fisrt_row_of_blocks[i]))
         
         #for i in range(0,channel_num):
         #    last_row[i] = current_row[i] #shallow copy
         last_row = current_row[:] #shallow copy
+    return result_out
 
-    # 还没有写计算过程，以及输出结果的address生成
 
+def output_generated_results(file_name, row_value_addr):
+    file_name_without_extension=file_name.split(".")[0]
+    output_dir = "../proceeded_data_sets/"+file_name_without_extension
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_file_name = output_dir+"/"+file_name_without_extension+"_result.txt"    
+    out_file = open(output_file_name, mode='w')
+    for data in row_value_addr:
+        print(data[0], data[1], data[2], file=out_file)
     return 0
-
 
 def main():
     file_name = "email-Eu-core.txt"
-    m_data, v_r_value_dict, v_r_addr_dict = readin(file_name)
-    process_element(m_data, v_r_value_dict, v_r_addr_dict)
+    m_data, v_r_value_dict, v_r_addr_dict, fisrt_row_of_blocks = readin(file_name)
+    results_vec = process_element(m_data, v_r_value_dict, v_r_addr_dict, fisrt_row_of_blocks)
+    results_vec = sorted(results_vec)
+
+    output_generated_results(file_name, results_vec)
 
 if __name__ == '__main__':
     main()
